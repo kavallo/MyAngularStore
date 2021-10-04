356fc102f902db270b00c9bd32710e94301d4a27
kavallo <luis.aneudy@gmail.com>
2021-10-04 16:21:22 -0400

    Page

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,12 +1,26 @@
 import { IProductos } from './models/productos';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-private SERVER_URL = "http://localhost:3000";
+  private SERVER_URL = "http://localhost:3000";
   constructor(private httpClient: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 
   public get() {
     console.log('servicio');
